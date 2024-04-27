263aedfaff00947563b7d26d2bdd557177b6795b
minc-nice-100 <wzc2162016@163.com>
2024-04-27 11:28:02 +0800

    对lib站点进行重构

--- allversion/js/logo.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @Date: 2023.07.25 18:07:31
- * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2023.07.25 18:07:51
- * @FilePath: \undefinedc:\Users\wzc21\Desktop\new\lib.github.io\allversion\images\logo.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-document.write('<img src="https://lib.ited.top/favicon.ico" alt="ited博客">');
