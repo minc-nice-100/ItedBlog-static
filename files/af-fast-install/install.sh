3b4f83e6f8affd9ef5b3bb37b1d33501ca6fdab2
minc-nice-100 <wzc2162016@163.com>
2025-07-18 00:22:46 +0800

    fix: 修正安装脚本中的文件名错误

--- files/af-fast-install/install.sh
+++ files/af-fast-install/install.sh
@@ -2,9 +2,9 @@
 # 从https://static.itedev.com/files/install-package.tar.gz下载安装包
 wget https://static.itedev.com/files/af-fast-install/package.tar.gz
 # 解压安装包
-tar -zxvf install-package.tar.gz
+tar -zxvf package.tar.gz
 # 进入安装包目录
-cd install-package
+cd package
 # 加权
 chmod +x installer/*
 # 执行安装脚本
